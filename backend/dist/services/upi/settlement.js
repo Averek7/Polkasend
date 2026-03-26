64656baa41bcb3cd96cdb074894973f73c6dd8d1
Avanish <singhavanish2003@gmail.com>
2026-03-26 10:55:38 +0530

    Remove generated backend artifacts

--- backend/dist/services/upi/settlement.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.triggerUpiSettlement = triggerUpiSettlement;
-async function triggerUpiSettlement(_request) {
-    // Stub for local/dev deployment. In production this calls banking rails.
-    return { accepted: true };
-}
